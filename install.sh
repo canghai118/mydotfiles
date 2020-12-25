ad9cdfc63e3454383757636363c69621be892d6f
zhujinyao <zhujinyao@corp-ci.com>
2020-12-25 14:08:48 +0800

    Add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+# install vim plugin - vundle
+echo "install vim vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install tmux plugin manager
+
+echo "install tmux plugin manager"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
